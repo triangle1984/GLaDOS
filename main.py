265501bcacbfc8a08e4f82fb7be8c7eaaa9ed5b2
anar66 <steam6456@gmail.com>
2019-09-16 14:19:26 +0000

    allowed users

--- main.py
+++ main.py
@@ -8,52 +8,53 @@ vk_session = vk_api.VkApi(token=token22)
 vk = vk_session.get_api()
 longpoll = VkLongPoll(vk_session)
 upload = vk_api.VkUpload(vk_session)
-otvet = None
+allowuser = [271595905, 367919273]
 for event in longpoll.listen():
     otvet = None
     if "text" in dir(event):
-        text = event.text.split()
-        try:
-            zapros = text[0].lower()
-        except IndexError:
-            continue
-        if zapros == "/калькулятор":
-            otvet = calc(text)
-        elif zapros == "/погода":
-            otvet = weather(text)
-        elif zapros == "слава":
-            otvet = {"message":"🇺🇦украине🇺🇦", "attachment":None}
-        elif zapros in ["привет", "ку", "зиг", "споки", "спокойной"]:
-            otvet = answer(text)
-        elif zapros == "/off" and event.user_id == 367919273:
-            sys.exit()
-        elif zapros == "/help" or zapros == "/хелп":
-            otvet = {"message":help, "attachment":None}
-        elif zapros == "/красилов":
-            vk.messages.send(user_id=event.user_id, random_id=get_random_id(),
-                             message="Krasyliv")
-        elif zapros == "/каты":
-            otvet = cats(vk)
-        # elif zapros == "/переводчик":
-        #     otvet = translit(text, vk)
-        elif zapros == "/юри":
-            otvet = yuri(vk)
-        elif zapros == "/геббельс":
-            otvet = gebbels(vk)
-        elif zapros == "/яой":
-            otvet = yaoi(vk)
-        elif zapros == "/трапы":
-            otvet = trap(vk)
-        elif zapros == "/лоли":
-            otvet = loli(vk)
-        elif zapros == "/оцени":
-            otvet = doulikethis(text)
-    if otvet:
-        if "chat_id" in dir(event):
-            vk.messages.send(chat_id=event.chat_id, random_id=get_random_id(),
-                             message="от бота: " + otvet["message"], attachment=otvet["attachment"])
-        else:
-            vk.messages.send(user_id=event.user_id, random_id=get_random_id(),
-                             message="от бота: " + otvet["message"], attachment=otvet["attachment"])
+        if event.user_id in allowuser:
+            text = event.text.split()
+            try:
+                zapros = text[0].lower()
+            except IndexError:
+                continue
+            if zapros == "/калькулятор":
+                otvet = calc(text)
+            elif zapros == "/погода":
+                otvet = weather(text)
+            elif zapros == "слава":
+                otvet = {"message":"🇺🇦украине🇺🇦", "attachment":None}
+            elif zapros in ["привет", "ку", "зиг", "споки", "спокойной"]:
+                otvet = answer(text)
+            elif zapros == "/off" and event.user_id == 367919273:
+                sys.exit()
+            elif zapros == "/help" or zapros == "/хелп":
+                otvet = {"message":help, "attachment":None}
+            elif zapros == "/красилов":
+                vk.messages.send(user_id=event.user_id, random_id=get_random_id(),
+                                message="Krasyliv")
+            elif zapros == "/каты":
+                otvet = cats(vk)
+            elif zapros == "/переводчик":
+                otvet = translit(text, vk)
+            elif zapros == "/юри":
+                otvet = yuri(vk)
+            elif zapros == "/геббельс":
+                otvet = gebbels(vk)
+            elif zapros == "/яой":
+                otvet = yaoi(vk)
+            elif zapros == "/трапы":
+                otvet = trap(vk)
+            elif zapros == "/лоли":
+                otvet = loli(vk)
+            elif zapros == "/оцени":
+                otvet = doulikethis(text)
+        if otvet:
+            if "chat_id" in dir(event):
+                vk.messages.send(chat_id=event.chat_id, random_id=get_random_id(),
+                                message="от бота: " + otvet["message"], attachment=otvet["attachment"])
+            else:
+                vk.messages.send(user_id=event.user_id, random_id=get_random_id(),
+                                message="от бота: " + otvet["message"], attachment=otvet["attachment"])
 
 
