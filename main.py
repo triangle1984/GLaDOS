418ae5dcced5f943b8b0febf7ca6a20617fa549f
anar66 <steam6456@gmail.com>
2019-09-19 21:48:00 +0000

    -c for photo

--- main.py
+++ main.py
@@ -5,93 +5,96 @@ from util import *
 from photo import *
 from smeh import *
 import vk_api, requests, sys
-vk_session = vk_api.VkApi(token=token22)
-vk = vk_session.get_api()
-longpoll = VkLongPoll(vk_session)
-upload = vk_api.VkUpload(vk_session)
-try:
-    for event in longpoll.listen():
-        response = {"message":None}
-        if "text" in dir(event) and "user_id" in dir(event):
-            if event.user_id in allowuser and "chat_id" not in dir(event):
-                text = event.text.split()
+def mainlobby():
+    global text
+    vk_session = vk_api.VkApi(token=token22)
+    vk = vk_session.get_api()
+    longpoll = VkLongPoll(vk_session)
+    upload = vk_api.VkUpload(vk_session)
+    try:
+        for event in longpoll.listen():
+            response = {"message":None}
+            if "text" in dir(event) and "user_id" in dir(event):
+                if event.user_id in allowuser and "chat_id" not in dir(event):
+                    text = event.text.split()
+                    try:
+                        requests = text[0].lower()
+                    except IndexError:
+                        continue
+                    if requests == "/калькулятор":
+                        response = calc(text)
+                    elif requests == "/погода":
+                        response = weather(text)
+                    elif requests == "слава":
+                        response = {"message":"🇺🇦украине🇺🇦", "attachment":None}
+                    elif requests in ["привет", "ку", "зиг", "споки", "спокойной"]:
+                        response = answer(text)
+                    elif requests == "/off" and event.user_id == 367919273:
+                        sys.exit()
+                    elif requests == "/help" or requests == "/хелп":
+                        response = {"message":help, "attachment":None}
+                    elif requests == "/красилов":
+                        vk.messages.send(user_id=event.user_id, random_id=get_random_id(),
+                                        message="Krasyliv")
+                    elif requests == "/каты":
+                        response = cats(vk,text)
+                    elif requests == "/переводчик":
+                        response = translit(text, vk)
+                    elif requests == "/юри":
+                        response = yuri(vk, text)
+                    elif requests == "/геббельс":
+                        response = gebbels(vk, text)
+                    elif requests == "/яой":
+                        response = yaoi(vk, text)
+                    elif requests == "/трапы":
+                        response = trap(vk, text)
+                    elif requests == "/лоли":
+                        response = loli(vk, text)
+                    elif requests == "/оцени":
+                        response = doulikethis(text)
+                    elif requests == "/вики":
+                        response = wiki(text)
+                    elif requests == "/махно":
+                        response = mahno(vk, text)
+                    elif requests == "/цитаты":
+                        response = citati(vk, text)
+                    elif requests == "/калян":
+                        response = colyan(vk, text)
+                    elif requests == "/видео":
+                        response = video(vk, text)
+                    elif requests == "/вероятность" or requests == "/шансы":
+                        response = chance(text)
+                    elif requests == "/выбери":
+                        response = oror(text)
+                    elif requests == "/смех":
+                        response = smex(text)
+                    elif requests == "/повтори":
+                        response = repeat(text)
+                    elif requests == "/док" or requests == "/гиф":
+                        response = rdocs(vk, text)
+                    elif requests == "/ноги" or requests == "/ножки":
+                        response = legs(vk,text)
+                    elif requests == "/мем":
+                        response = mem(vk, text)
+                    elif requests == "/кто":
+                        response = who(vk, event, text)
+                    elif requests == "/курс":
+                        response = valute(text)
+                    elif requests == "/дата":
+                        response = date(text)
+                    elif requests == "/число":
+                        response = number(text)
                 try:
-                    requests = text[0].lower()
-                except IndexError:
+                    if response["message"]:
+                        if "attachment" not in response:
+                            response["attachment"] = None
+                        # if "chat_id" in dir(event):
+                        #     vk.messages.send(chat_id=event.chat_id, random_id=get_random_id(),
+                        #                     message="от бота: " + response["message"], attachment=response["attachment"])
+                        vk.messages.send(user_id=event.user_id, random_id=get_random_id(),
+                                        message="от бота: " + response["message"], attachment=response["attachment"])
+                except TypeError:
                     continue
-                if requests == "/калькулятор":
-                    response = calc(text)
-                elif requests == "/погода":
-                    response = weather(text)
-                elif requests == "слава":
-                    response = {"message":"🇺🇦украине🇺🇦", "attachment":None}
-                elif requests in ["привет", "ку", "зиг", "споки", "спокойной"]:
-                    response = answer(text)
-                elif requests == "/off" and event.user_id == 367919273:
-                    sys.exit()
-                elif requests == "/help" or requests == "/хелп":
-                    response = {"message":help, "attachment":None}
-                elif requests == "/красилов":
-                    vk.messages.send(user_id=event.user_id, random_id=get_random_id(),
-                                    message="Krasyliv")
-                elif requests == "/каты":
-                    response = cats(vk)
-                elif requests == "/переводчик":
-                    response = translit(text, vk)
-                elif requests == "/юри":
-                    response = yuri(vk)
-                elif requests == "/геббельс":
-                    response = gebbels(vk)
-                elif requests == "/яой":
-                    response = yaoi(vk)
-                elif requests == "/трапы":
-                    response = trap(vk)
-                elif requests == "/лоли":
-                    response = loli(vk)
-                elif requests == "/оцени":
-                    response = doulikethis(text)
-                elif requests == "/вики":
-                    response = wiki(text)
-                elif requests == "/махно":
-                    response = mahno(vk)
-                elif requests == "/цитаты":
-                    response = citati(vk)
-                elif requests == "/калян":
-                    response = colyan(vk)
-                elif requests == "/видео":
-                    response = video(vk, text)
-                elif requests == "/вероятность" or requests == "/шансы":
-                    response = chance(text)
-                elif requests == "/выбери":
-                    response = oror(text)
-                elif requests == "/смех":
-                    response = smex(text)
-                elif requests == "/повтори":
-                    response = repeat(text)
-                elif requests == "/док" or requests == "/гиф":
-                    response = rdocs(vk, text)
-                elif requests == "/ноги" or requests == "/ножки":
-                    response = legs(vk)
-                elif requests == "/мем":
-                    response = mem(vk)
-                elif requests == "/кто":
-                    response = who(vk, event, text)
-                elif requests == "/курс":
-                    response = valute(text)
-                elif requests == "/дата":
-                    response = date(text)
-                elif requests == "/число":
-                    response = number(text)
-            try:
-                if response["message"]:
-                    if "attachment" not in response:
-                        response["attachment"] = None
-                    # if "chat_id" in dir(event):
-                    #     vk.messages.send(chat_id=event.chat_id, random_id=get_random_id(),
-                    #                     message="от бота: " + response["message"], attachment=response["attachment"])
-                    vk.messages.send(user_id=event.user_id, random_id=get_random_id(),
-                                    message="от бота: " + response["message"], attachment=response["attachment"])
-            except TypeError:
-                continue
-except KeyboardInterrupt:
-    sys.exit()
+    except KeyboardInterrupt:
+        sys.exit()
+mainlobby()
--- photo.py
+++ photo.py
@@ -1,45 +1,57 @@
-import vk_api, requests, random
+import vk_api, requests, random, argparse
 from vk_api.utils import get_random_id
-def phootowallrandom(groups, vk, albid="wall"):
-    group_id = random.choice(groups)
-    max_num = vk.photos.get(owner_id=group_id, album_id=albid, count=0)['count']
-    num = random.randint(0, max_num)
-    photo = vk.photos.get(owner_id=group_id, album_id=albid,
-                          count=1, offset=num)['items'][0]['id']
-    return f"photo{group_id}_{photo}"
-def yuri(vk):
-    photo = phootowallrandom(["-170165000", "-63092480", "-153284406"], vk)
+def args2():
+    args = argparse.ArgumentParser(description="картинки")
+    args.add_argument("-c", "--count", type=int, default=1)
+    return args
+def phootowallrandom(groups, vk, text, albid="wall"):
+    a = args2()
+    a = a.parse_args(text[1:])
+    photo2 = []
+    if a.count > 10:
+        a.count = 10
+    for _ in range(a.count):
+        group_id = random.choice(groups)
+        max_num = vk.photos.get(owner_id=group_id, album_id=albid, count=0)['count']
+        num = random.randint(0, max_num)
+        photo = vk.photos.get(owner_id=group_id, album_id=albid,
+                            count=1, offset=num)['items'][0]['id']
+        photo2.append(f"photo{group_id}_{photo}")
+    photo2 = ",".join(photo2)
+    return photo2
+def yuri(vk, text):
+    photo = phootowallrandom(["-170165000", "-63092480", "-153284406"], vk, text)
     return {"message":"Юрец~~🌚", "attachment":photo}
-def gebbels(vk):
-    photo = phootowallrandom(["-174482230"], vk)
+def gebbels(vk, text):
+    photo = phootowallrandom(["-174482230"], vk, text)
     return {"message":"ХАЧЕШЬ ЛИ ТЫ ТОТАЛЬНОЙ ВАЙНЫ?", "attachment":photo}
-def yaoi(vk):
-    photo = phootowallrandom(["-98467405", "-113004231", "-57807542", "-38230251"], vk)
+def yaoi(vk, text):
+    photo = phootowallrandom(["-98467405", "-113004231", "-57807542", "-38230251"], vk, text)
     return {"message":"Яойчег~~🌚", "attachment":photo}
-def trap(vk):
-    photo = phootowallrandom(["-171834188"], vk)
+def trap(vk , text):
+    photo = phootowallrandom(["-171834188"], vk, text)
     return {"message":"Трапы~~🌚", "attachment":photo}
-def cats(vk):
-    photo = phootowallrandom(["-43228812", "-34137527", "-152424758"], vk)
+def cats(vk , text):
+    photo = phootowallrandom(["-43228812", "-34137527", "-152424758"], vk, text)
     return {"message":"Шавухенция на заказ", "attachment":photo}
-def loli(vk):
-    photo = phootowallrandom(["-127518015", "-157516431", "-69721869"], vk)
+def loli(vk , text):
+    photo = phootowallrandom(["-127518015", "-157516431", "-69721869"], vk, text)
     return {"message":"FBI OPEN UP", "attachment":photo}
-def mahno(vk):
-    photo = phootowallrandom(["367919273"],vk,  albid=262361216)
+def mahno(vk , text):
+    photo = phootowallrandom(["367919273"],vk, text, albid=262361216)
     return {"message":"СВОБОДА АБО ИДИТЕ НАХУЙ", "attachment":photo}
-def citati(vk):
-    photo = phootowallrandom(["-173186389"], vk)
+def citati(vk , text):
+    photo = phootowallrandom(["-173186389"], vk, text)
     return {"message":"Цитатки на заказ", "attachment":photo}
-def colyan(vk):
-    photo = phootowallrandom(["-183493220"],vk,  albid=266695546)
+def colyan(vk , text):
+    photo = phootowallrandom(["-183493220"],vk, text,  albid=266695546)
     return {"message":"БОЖЕЕЕЕЕЕЕ, ЦАРЯ ХРАНИ", "attachment":photo}
-def hentai(vk):
-    photo = phootowallrandom(["-161403814", "-170993976"], vk)
+def hentai(vk , text):
+    photo = phootowallrandom(["-161403814", "-170993976"], vk, text)
     return {"message":"Хентай~~🌚", "attachment":photo}
-def legs(vk):
-    photo = phootowallrandom(["-174842315", "-102853758", "-134982584", "-138265009", "-114279288"], vk)
+def legs(vk , text):
+    photo = phootowallrandom(["-174842315", "-102853758", "-134982584", "-138265009", "-114279288"], vk, text)
     return {"message": "Ножки &#127773;", "attachment": photo}
-def mem(vk):
-    photo = phootowallrandom(["-154306815", "-142918020", "-120254617", "-79805359", "-150550417"], vk)
+def mem(vk  , text):
+    photo = phootowallrandom(["-154306815", "-142918020", "-120254617", "-79805359", "-150550417"], vk, text)
     return {"message": "Держи мемас", "attachment": photo}
