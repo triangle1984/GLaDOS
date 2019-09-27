c31e9c80b52cb86dfe53413003b4d0e0dd04671d
anar66 <steam6456@gmail.com>
2019-09-27 16:28:05 +0000

    oops

--- vk_bot/botutil.py
+++ vk_bot/botutil.py
@@ -2,6 +2,6 @@ from token2 import recipient
 from vk_api.utils import get_random_id
 def groupjoin(vk, event):
     idjoin = f"*id{event.object['user_id']}"
-    vk.messages.send(user_id=recipient, random_id=get_random_id(), attachment="photo271595905_457261640"
+    vk.messages.send(user_id=recipient, random_id=get_random_id(), attachment="photo271595905_457261640",
                         message=f'В группу вступил новый пользователь! {idjoin}')
 
