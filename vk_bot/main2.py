4f00f3306dc7128bec637600cc1bd2e247d83d7b
root <root@triangle-vk2.lxd>
2019-10-11 14:48:32 +0000

    добавил givemoney

--- vk_bot/main2.py
+++ vk_bot/main2.py
@@ -22,6 +22,7 @@ def lobby(vk,vk2, mc, event):
         text = event.object.text.split()
         uid = event.object.from_id
         mc2 = sqlcache(mc, uid)
+        givemoney(uid,mc2)
         if mc2["ban"]:
             return
         try:
