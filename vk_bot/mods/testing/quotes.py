224a91d676cb80f518ef03f9c79ab154eaaf43d3
anar66 <steam6456@gmail.com>
2019-10-26 23:42:01 +0000

    config font

--- vk_bot/mods/testing/quotes.py
+++ vk_bot/mods/testing/quotes.py
@@ -1,8 +1,6 @@
 from PIL import Image, ImageDraw, ImageFont
 from loadevn import *
-import textwrap
-import io
-import requests
+import textwrap, io, requests, random, os
 from vk_bot.modutil import BacisPlug
 class Quote(BacisPlug):
     doc = "Сделать цитату по пересланному сообщению"
@@ -13,8 +11,8 @@ class Quote(BacisPlug):
         MAX_W, MAX_H = 700, 400
         im = Image.new('RGB', (MAX_W, MAX_H), (0, 0, 0, 0))
         draw = ImageDraw.Draw(im)
-        font = ImageFont.truetype('/usr/share/fonts/TTF/DejaVuSansCode-Bold.ttf',20)
-        fontu = ImageFont.truetype('/usr/share/fonts/TTF/DejaVuSansCode-Bold.ttf',18)
+        font = ImageFont.truetype(fontc,20)
+        fontu = ImageFont.truetype(fontc,18)
         current_h, pad = 150, 10
         for line in para:
             w, h = draw.textsize(line, font=font)
@@ -29,5 +27,8 @@ class Quote(BacisPlug):
         firstname = self.vk.users.get(user_ids=self.event.object.reply_message['from_id'])[0]['first_name']
         lastname =  self.vk.users.get(user_ids=self.event.object.reply_message['from_id'])[0]['last_name']
         draw.text((30, 310), f'{firstname} {lastname}', font=fontu)
-        im.save('test.jpg')
-        print(".")
+        name = f"name{random.randint(0, 1000)}.jpg"
+        im.save(name)
+        attachment  = self.uploadphoto(name)
+        self.sendmsg("Дэржите цитатку", attachment)
+        os.remove(name)
