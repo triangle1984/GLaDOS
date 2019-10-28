e52281288fa6965696611f5e12ae085ec9a895ef
anar66 <steam6456@gmail.com>
2019-10-28 12:50:34 +0000

    цитата фон. Пока шо говнокод, но доделаю

--- vk_bot/mods/testing/quotes.py
+++ vk_bot/mods/testing/quotes.py
@@ -7,32 +7,47 @@ class Quote(BacisPlug):
     doc = "Сделать цитату по пересланному сообщению"
     command = ["/цитата", "/цитаты"]
     def __checkbackground(self):
-        pass
+        MAX_W, MAX_H = 700, 400
+        check = os.path.exists(f"photos/{self.uid}")
+        if check:
+           self.im = Pillowhelper.resize_image(f'photos/{self.uid}', (MAX_W, MAX_H))
+        else:
+            self.im = Image.new('RGB', (MAX_W, MAX_H), (0, 0, 0, 0))
+    def __setbackground(self):
+        try:
+            url = self.msg['attachments'][0]['photo']['sizes'][-1]['url']
+        except:
+            self.sendmsg(f"а пикчу вы видимо забыли")
+        os.system(f'wget {url} -O photos/{self.uid}')
     def main(self):
         try:
             if not self.event.object.fwd_messages:
-                msg = self.event.object.reply_message
-                astr = msg['text']
+                self.msg = self.event.object.reply_message
+                astr = self.msg['text']
             else:
-                msg = self.event.object.fwd_messages[0]
-                msgl = self.event.object.fwd_messages
+                self.msg = self.event.object.fwd_messages[0]
+                self.msgl = self.event.object.fwd_messages
                 astrlist = []
-                uid = msg["from_id"]
-                for a in msgl:
+                uid = self.msg["from_id"]
+                for a in self.msgl:
                     if a["from_id"] == uid:
                         astrlist.append(a['text'])
                 astr = "\n".join(astrlist)
-            url = self.vk.users.get(user_ids=msg['from_id'], fields='photo_max')[0]['photo_max']
-            firstname = self.vk.users.get(user_ids=msg['from_id'])[0]['first_name']
-            lastname =  self.vk.users.get(user_ids=msg['from_id'])[0]['last_name']
-        except:
+            self.uid = self.msg['from_id']
+            url = self.vk.users.get(user_ids=self.msg['from_id'], fields='photo_max')[0]['photo_max']
+            firstname = self.vk.users.get(user_ids=self.msg['from_id'])[0]['first_name']
+            lastname =  self.vk.users.get(user_ids=self.msg['from_id'])[0]['last_name']
+        except KeyboardInterrupt:
             self.sendmsg("!error")
             return
+        if len(self.text) > 1:
+            if self.text[1] == "фон":
+                self.__setbackground()
+                return
+        self.__checkbackground()
         today = datetime.datetime.today().strftime("время: %H:%M:%S, дата: %Y-%m-%d")
         para = textwrap.wrap(astr, width=30)
-        MAX_W, MAX_H = 700, 400
-        im = Image.new('RGB', (MAX_W, MAX_H), (0, 0, 0, 0))
-        draw = ImageDraw.Draw(im)
+        draw = ImageDraw.Draw(self.im)
         font = ImageFont.truetype(fontc,16)
         fontu = ImageFont.truetype(fontc,14)
         current_h, pad = 130, 10
@@ -41,15 +56,15 @@ class Quote(BacisPlug):
             draw.text((250, current_h), line, font=font)
             current_h += h + pad
 
-        img = requests.get(url).content
-        f = io.BytesIO(img)
+        self.img = requests.get(url).content
+        f = io.BytesIO(self.img)
 
         watermark = Image.open(f).convert("RGBA")
-        im.paste(watermark, (10, 100),  watermark)
+        self.im.paste(watermark, (10, 100),  watermark)
         draw.text((10, 310), f'{firstname} {lastname}', font=fontu)
         draw.text((10, 325), today, font=fontu)
         name = f"name{random.randint(0, 1000)}.jpg"
-        im.save(name)
+        self.im.save(name)
         try:
             attachment  = self.uploadphoto(name)
             self.sendmsg("Дэржите цитатку", attachment)
