e2005a662bc77448eb2757fd57687cf0d0e91e10
anar66 <steam6456@gmail.com>
2019-10-28 08:33:32 +0000

    блять

---
+++ createtabes.py
@@ -0,0 +1,32 @@
+import pymysql
+from pymysql.cursors import DictCursor
+from contextlib import closing
+from vk_bot.core.sql.vksql import auth
+from loadevn import *
+conn = auth()
+with conn.cursor() as cursor:
+    query = "CREATE TABLE IF NOT EXISTS admins (id int)"
+    cursor.execute(query)
+    query = "CREATE TABLE IF NOT EXISTS ban (id int)"
+    cursor.execute(query)
+    query = f"CREATE TABLE IF NOT EXISTS {tablechat} (id int)"
+    cursor.execute(query)
+    query = f"CREATE TABLE IF NOT EXISTS prefix (id int, name varchar(40))"
+    cursor.execute(query)
+    query = f"CREATE TABLE IF NOT EXISTS smehgen (id int, count int, smeh varchar(40), smehslova varchar(40))"
+    cursor.execute(query)
+    query = f"CREATE TABLE IF NOT EXISTS prefix (id int, name varchar(40))"
+    cursor.execute(query)
+    query = f"CREATE TABLE IF NOT EXISTS {chathello} (id int)"
+    cursor.execute(query)
+    query = f"CREATE TABLE IF NOT EXISTS relation (id int, id2 int)"
+    cursor.execute(query)
+    query = "CREATE TABLE IF NOT EXISTS vips (id int)"
+    cursor.execute(query)
+    query = "CREATE TABLE IF NOT EXISTS yourphoto (id int, command varchar(40), public varchar(666), number int)"
+    cursor.execute(query)
+    query = "CREATE TABLE IF NOT EXISTS yourgroup (id int, command varchar(40), public varchar(666), number int)"
+    cursor.execute(query)
+    query = "CREATE TABLE IF NOT EXISTS economy (id int, money int)"
+    cursor.execute(query)
+    conn.commit()
