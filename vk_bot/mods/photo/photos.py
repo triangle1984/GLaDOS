b27e09a0b27da02b1deb516461910412efb3610b
anar66 <steam6456@gmail.com>
2019-10-31 22:34:02 +0000

    больше коммитов о хелпе2 богу коммитов о хелпе2. Терь отображаются все команды, а не только первая и уменьшил размер

---
+++ vk_bot/mods/photo/photos.py
@@ -0,0 +1,9 @@
+from PIL import Image, ImageDraw
+from loadevn import *
+import io, requests, random, os
+from vk_bot.core.utils.modutil import BacisPlug
+class Quote(BacisPlug):
+    doc = "Черно-белый фильтр"
+    command = ["/чб"]
+    def main(self):
+        pass
--- vk_bot/mods/util/help.py
+++ vk_bot/mods/util/help.py
@@ -8,17 +8,16 @@ class GetHelp(BacisPlug):
         lhelp = []
         mhelp = "\n"
         for moduli in mods.modules:
-            lhelp.append(dict(command=moduli.command[0], doc=moduli.doc))
-        lhelp = list(iterutils.chunked_iter(lhelp, 13))
+            lhelp.append(dict(command=moduli.command, doc=moduli.doc))
+        lhelp = list(iterutils.chunked_iter(lhelp, 9))
         try:
             number = int(self.text[1])
             lhelp2 = lhelp[number]
         except:
-            self.sendmsg("такой страницы нет. Смотрим 0ую")
             number = 0
             lhelp2 = lhelp[0]
         for moduli in lhelp2:
-            mhelp += f"• {moduli['command']} - {moduli['doc']} \n"
+            mhelp += f"• {', '.join(moduli['command'])} - {moduli['doc']} \n"
         mhelp += f"Страница: {number} \n"
         mhelp += f"Всего страниц: {len(lhelp)-1}"
         self.sendmsg(mhelp)
