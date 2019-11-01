c9d8b2628e3cecac5c7aeb2eff37e41bc61b8d98
anar66 <steam6456@gmail.com>
2019-11-01 14:23:09 +0000

    оботе

---
+++ vk_bot/mods/bots/about.py
@@ -0,0 +1,15 @@
+from vk_bot.core.utils.modutil import BacisPlug
+from vk_bot import mods
+from boltons import iterutils
+class AboutBot(BacisPlug):
+    doc = "Инфа о боте"
+    command = ["/оботе"]
+    def main(self):
+        info = """
+        бот написан на python, двумя прогерами
+        первый прогер: *slava_a_i_rA
+        второй прогер: *pythonoglot
+        модуль на распознавание гс написал: *feelan03
+        Приятного пользования~
+        """
+        self.sendmsg(info)
