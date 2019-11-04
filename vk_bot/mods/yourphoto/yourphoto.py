3f26ca123839889aead8f684ad69b713f30a5332
anar66 <steam6456@gmail.com>
2019-11-04 23:14:35 +0000

    рандомный айди альбомов, ежели не указывать

--- vk_bot/mods/yourphoto/yourphoto.py
+++ vk_bot/mods/yourphoto/yourphoto.py
@@ -1,34 +1,47 @@
 from vk_bot.core.sql.vksql import *
 from vk_bot.core.modules.basicplug import BasicPlug
 from vk_bot.core.modules.othermethods import OtherMethod
+import random
+
+
 class Yourphoto(BasicPlug, OtherMethod):
     command = ["/альбомы"]
     doc = "Личные альбомы, вызов без всего покажет справку"
     types = 'specialcommand'
 
     def main(self):
-        number = 1
+        number = random.randint(0, 9999999)
         try:
             if self.text[1] == "список":
-                self.sendmsg(self.getyourphoto());return
+                self.sendmsg(self.getyourphoto())
+                return
             elif self.text[1] == "удалить":
-                self.sendmsg(self.rmyourphoto());return
+                self.sendmsg(self.rmyourphoto())
+                return
             else:
                 command = self.text[1].lower()
-                public = "".join(self.text[2:]);public = public.split(",")
+                public = "".join(self.text[2:])
+                public = public.split(",")
                 public = ",".join(self.nametoid(public))
                 number = "".join(self.text[0])[8:]
                 number = int(number)
-                if self.mc2["vips"] == False and mc["count"] >=3:
-                    self.sendmsg("А больше трех альбомов юзерам низя");return
+                if self.mc2["vips"] == False and mc["count"] >= 3:
+                    self.sendmsg("А больше трех альбомов юзерам низя")
+                    return
         except IndexError:
-            self.sendmsg("Гайд по альбомам: https://self.vk.com/@mtt_resort-gaid-po-lichnym-albomam");return
+            self.sendmsg(
+                "Гайд по альбомам: https://self.vk.com/@mtt_resort-gaid-po-lichnym-albomam")
+            return
         except ValueError:
-            number = 1
-        if checktable("yourphoto","id", self.uid, andd=f"number = {number}"):
+            number = random.randint(0, 9999999)
+        if checktable("yourphoto", "id", self.uid, andd=f"number = {number}"):
             tablerm("yourphoto", "id", self.uid, andd=f"number = {number}")
-        tableadd("yourphoto", "id,command,public,number",f"{self.uid}, '{command}','{public}', '{number}'")
-        self.sendmsg(f"Ваш личный альбом №{number} настроен, паблики: {public}, команда: {command}");return
+        tableadd("yourphoto", "id,command,public,number",
+                 f"{self.uid}, '{command}','{public}', '{number}'")
+        self.sendmsg(
+            f"Ваш личный альбом №{number} настроен, паблики: {public}, команда: {command}")
+        return
+
     def getyourphoto(self):
         conn = auth()
         total = "\n"
@@ -38,6 +51,7 @@ class Yourphoto(BasicPlug, OtherMethod):
             for row in cursor:
                 total += f"Команда: {row['command']}, паблики: {row['public']}, айди: {row['number']}\n"
             return total
+
     def rmyourphoto(self):
         number = self.text[2]
         if number != "все":
