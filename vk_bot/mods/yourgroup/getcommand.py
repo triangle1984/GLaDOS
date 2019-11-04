915df6893e2a0239c954f58f9888ad8f8152f79b
anar66 <steam6456@gmail.com>
2019-11-04 14:02:32 +0000

    даня я починил твои группы

--- vk_bot/mods/yourgroup/getcommand.py
+++ vk_bot/mods/yourgroup/getcommand.py
@@ -1,7 +1,7 @@
 from vk_bot.core.utils.modutil import BacisPlug
 from vk_bot.core.sql.vksql import *
+import random
 class Getcommand(BacisPlug):
-    doc = "вытащить команду"
     types = 'commandb'
     @staticmethod
     def getcommand(uid, command):
@@ -15,22 +15,17 @@ class Getcommand(BacisPlug):
     def main(self):
         check = checktable("yourgroup", "id", self.uid, andd=f"command = '{self.text[0]}'")
         if check:
-            post = Group(self.vk2, self.text)
             public = check["public"]
             public = public.split(",")
-            posts = posts.yourpost(public)
+            posts = self.yourpost(public)
             self.sendmsg("Рандомный пост", posts)
-class Group:
-    def __init__(self, vk,text):
-        self.vk = vk
-        self.text = text
     def groupwallrandom(self, groups, albid="wall"):
         post2 = []
         try:
             group_id = random.choice(groups)
-            max_num = self.vk.wall.get(owner_id=group_id, album_id=albid, count=0)['count']
+            max_num = self.vk2.wall.get(owner_id=group_id, album_id=albid, count=0)['count']
             num = random.randint(0, max_num)
-            post = self.vk.wall.get(owner_id=group_id, count=1, offset=num)['items'][0]['id']
+            post = self.vk2.wall.get(owner_id=group_id, count=1, offset=num)['items'][0]['id']
             post2.append(f"wall{group_id}_{post}")
         except KeyboardInterrupt:
             self.sendmsg("!error от вк");return
