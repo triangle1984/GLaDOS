4f77c0013c50f2f57bdccea042ba0bad7f9fdd63
Ferowenso <ferowenso@yandex.ru>
2019-11-04 12:52:54 +0300

    крч развлекайся

--- vk_bot/mods/yourgroup/getcommand.py
+++ vk_bot/mods/yourgroup/getcommand.py
@@ -1,5 +1,4 @@
 from vk_bot.core.utils.modutil import BacisPlug
-from group import Group
 from vk_bot.core.sql.vksql import *
 class Getcommand(BacisPlug):
     doc = "вытащить команду"
@@ -21,3 +20,22 @@ class Getcommand(BacisPlug):
             public = public.split(",")
             posts = posts.yourpost(public)
             self.sendmsg("Рандомный пост", posts)
+class Group:
+    def __init__(self, vk,text):
+        self.vk = vk
+        self.text = text
+    def groupwallrandom(self, groups, albid="wall"):
+        post2 = []
+        try:
+            group_id = random.choice(groups)
+            max_num = self.vk.wall.get(owner_id=group_id, album_id=albid, count=0)['count']
+            num = random.randint(0, max_num)
+            post = self.vk.wall.get(owner_id=group_id, count=1, offset=num)['items'][0]['id']
+            post2.append(f"wall{group_id}_{post}")
+        except KeyboardInterrupt:
+            self.sendmsg("!error от вк");return
+        photo2 = ",".join(post2)
+        return photo2
+    def yourpost(self, public):
+        post = self.groupwallrandom(public)
+        return post
