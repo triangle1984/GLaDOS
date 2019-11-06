c85aaab896aec0e563266ab8d907df04ed63409d
anar66 <steam6456@gmail.com>
2019-11-06 20:36:08 +0000

    join забыл

--- vk_bot/mods/photo/getrule34.py
+++ vk_bot/mods/photo/getrule34.py
@@ -7,7 +7,7 @@ class GetRule34(BasicPlug, OtherMethod):
     def main(self):
         url = "https://r34-json-api.herokuapp.com/posts"
         params = {
-            "tags": self.text[1:],
+            "tags": "".join(self.text[1:]),
             "limit": 50
         }
         r = requests.get(url, params=params).json()
