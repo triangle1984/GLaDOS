177226471456705a54268fcadefa97adfa153072
anar66 <steam6456@gmail.com>
2019-11-07 16:00:45 +0000

    забыл убрать

--- vk_bot/mods/photo/nekoslife.py
+++ vk_bot/mods/photo/nekoslife.py
@@ -13,6 +13,6 @@ class Nekoslife(BasicPlug, OtherMethod):
                 self.sendmsg("Держи!", self.uploaddoc(image.raw, peer_id=self.peer_id))
             else:
                 self.sendmsg("Держи!", self.uploadphoto(image.raw))
-        except KeyboardInterrupt:
+        except:
             self.sendmsg("""Введи один из этих аргументов:
             feet, yuri, trap, futanari, hololewd, lewdkemo, solog, feetg, cum, erokemo, les, wallpaper, lewdk, ngif, tickle, lewd, feed, gecg, eroyuri, eron, cum_jpg, bj, nsfw_neko_gif, solo, kemonomimi, nsfw_avatar, gasm, poke, anal, slap, hentai, avatar, erofeet, holo, keta, blowjob, pussy, tits, holoero, lizard, pussy_jpg, pwankg, classic, kuni, waifu, pat, 8ball, kiss, femdom, neko, spank, cuddle, erok, fox_girl, boobs, random_hentai_gif, smallboobs, hug, ero, smug, goose, baka""")
