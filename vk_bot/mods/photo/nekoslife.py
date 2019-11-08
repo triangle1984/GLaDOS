c7b6f572c29e22b91d9567c48b4bab254969dbd0
anar66 <steam6456@gmail.com>
2019-11-08 19:50:28 +0000

    се-се, последний коммит о некосе

--- vk_bot/mods/photo/nekoslife.py
+++ vk_bot/mods/photo/nekoslife.py
@@ -21,5 +21,5 @@ class Nekoslife(BasicPlug, Upload, OtherMethod):
             image = self.multithreadwoload(image_url)
             self.sendmsg("Держи!", image)
         except:
-            self.sendmsg("""Введи один из этих аргументов:
+            self.sendmsg("""Введи один из этих аргументов, с цифрой на конце или с -c 5 - скинет указанное количество пикч:
             feet, yuri, trap, futanari, hololewd, lewdkemo, solog, feetg, cum, erokemo, les, wallpaper, lewdk, ngif, tickle, lewd, feed, gecg, eroyuri, eron, cum_jpg, bj, nsfw_neko_gif, solo, kemonomimi, nsfw_avatar, gasm, poke, anal, slap, hentai, avatar, erofeet, holo, keta, blowjob, pussy, tits, holoero, lizard, pussy_jpg, pwankg, classic, kuni, waifu, pat, 8ball, kiss, femdom, neko, spank, cuddle, erok, fox_girl, boobs, random_hentai_gif, smallboobs, hug, ero, smug, goose, baka""")
