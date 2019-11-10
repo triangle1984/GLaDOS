4428bd1b49087f59ddbeabf3e074a82ef4ead81a
anar66 <steam6456@gmail.com>
2019-11-10 16:51:44 +0000

    поднял лимит некоса для випов, возможно ето буит глобальным

--- vk_bot/mods/photo/nekoslife.py
+++ vk_bot/mods/photo/nekoslife.py
@@ -3,6 +3,7 @@ from vk_bot.core.modules.upload import Upload
 from vk_bot.core.modules.othermethods import OtherMethod
 import requests
 import nekos
+from boltons import iterutils
 
 
 class Nekoslife(BasicPlug, Upload, OtherMethod):
@@ -11,15 +12,21 @@ class Nekoslife(BasicPlug, Upload, OtherMethod):
 
     def main(self):
         a = self.args(self.text[2:])
-        if a.count > 10:
+        if a.count > 10 and self.mc2['vips'] == False:
             a.count = 10
+        elif a.count > 30:
+            a.count = 30
         image_url = []
         try:
             for _ in range(a.count):
                 image_url.append(nekos.img(self.text[1]))
-            self.sendmsg("начинаю качать пикчи")
+            if a.count > 4:
+                self.sendmsg("начинаю качать пикчи")
             image = self.multithreadwoload(image_url)
-            self.sendmsg("Держи!", image)
+            image = list(iterutils.chunked_iter(image.split(","), 10))
+            for image in image:
+                images = ",".join(image)
+                self.sendmsg("Держи!", images)
         except:
             self.sendmsg("""Введи один из этих аргументов, с цифрой на конце или с -c 5 - скинет указанное количество пикч:
             feet, yuri, trap, futanari, hololewd, lewdkemo, solog, feetg, cum, erokemo, les, wallpaper, lewdk, ngif, tickle, lewd, feed, gecg, eroyuri, eron, cum_jpg, bj, nsfw_neko_gif, solo, kemonomimi, nsfw_avatar, gasm, poke, anal, slap, hentai, avatar, erofeet, holo, keta, blowjob, pussy, tits, holoero, lizard, pussy_jpg, pwankg, classic, kuni, waifu, pat, 8ball, kiss, femdom, neko, spank, cuddle, erok, fox_girl, boobs, random_hentai_gif, smallboobs, hug, ero, smug, goose, baka""")
