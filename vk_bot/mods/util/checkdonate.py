5974c96ba359b4e2943705b04a8e6343ced443fc
anar66 <steam6456@gmail.com>
2019-11-12 14:02:12 +0000

    вроде бы фикс доната

--- vk_bot/mods/util/checkdonate.py
+++ vk_bot/mods/util/checkdonate.py
@@ -2,18 +2,27 @@ from vk_bot.core.modules.basicplug import BasicPlug
 import requests
 from loadevn import donatetoken
 from vk_bot.core.sql.vksql import tableadd
+
+
 class Checkdonate(BasicPlug):
     doc = "Проверить донатил ли юзер"
     command = ["/чекнидонат"]
+
     def main(self):
         url = f"https://api.vkdonate.ru?action=donates&count=50&sort=sum&key={donatetoken}"
         r = requests.get(url)
         text = r.json()
         msg = "ниеть, донатов от вас не было"
         attachments = '157509098_456239021'
+        udonate = False
         for donate in text['donates']:
             if donate["uid"] == self.uid:
-                attachments = 'video139157356_456239665'
-                msg = f"а вы мисье донатер. Надонатил: {donate['sum']}₽. Терь вы с випкой"
-                tableadd("vips", "id", self.uid, one=True)
-        self.sendmsg(msg, attachments)
+                udonate += donate['sum']
+        if udonate:
+            tableadd("vips", "id", self.uid, one=True)
+            self.sendmsg(
+                f"а вы мисье донатер. Надонатил: {udonate}₽. Терь вы с випкой",
+                "video139157356_456239665")
+        else:
+            self.sendmsg("Ниеть, донатов от вас не было",
+                         'video157509098_456239021')
