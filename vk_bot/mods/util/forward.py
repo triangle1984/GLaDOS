1b636a5ae53d25457a5d5ee31c60526ca70e9b73
anar66 <steam6456@gmail.com>
2019-11-12 19:15:49 +0000

    upload захватит мир

--- vk_bot/mods/util/forward.py
+++ vk_bot/mods/util/forward.py
@@ -1,17 +1,15 @@
 import requests
 from vk_bot.core.modules.basicplug import BasicPlug
-from vk_bot.core.modules.othermethods import OtherMethod
-class Forward(BasicPlug, OtherMethod):
+from vk_bot.core.modules.upload import Upload
+
+
+class Forward(BasicPlug, Upload):
     doc = "Пересылает фотографию"
     command = ["/перешли"]
+
     def main(self):
         try:
-            attachments = []
-            session = requests.Session()
             image_url = self.event.object['attachments'][0]['photo']['sizes'][-1]['url']
-            image = session.get(image_url, stream=True)
-            photo = self.upload.photo_messages(photos=image.raw)[0]
-            attachments.append('photo{}_{}'.format(photo['owner_id'], photo['id']))
-            self.sendmsg("Держи!", ','.join(attachments))
+            self.sendmsg("Держи!", self.dowloadupload(image_url))
         except IndexError:
             self.sendmsg("Мне нужно фото!")
