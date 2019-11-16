a690614ba43fbf99cb0815e8fdec84e01ff86b74
anar66 <steam6456@gmail.com>
2019-11-16 11:42:14 +0000

    бля > и < перепутал

--- vk_bot/mods/games/hug.py
+++ vk_bot/mods/games/hug.py
@@ -9,7 +9,7 @@ class Hug(BasicPlug, Upload):
     doc = "обнять юзера"
 
     def main(self):
-        if len(self.text) < 1:
+        if len(self.text) > 1:
             attachment = self.dowloadupload(nekos.img("hug"))
             gender = self.vk.users.get(
                 user_ids=self.uid, fields="sex")[0]['sex']
