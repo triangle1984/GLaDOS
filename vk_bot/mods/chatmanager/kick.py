b225552cde17ca5b13fb767e64c3f522ea638fa2
anar66 <steam6456@gmail.com>
2019-11-17 12:02:51 +0000

    итс тайм фор рефакторинг

--- vk_bot/mods/chatmanager/kick.py
+++ vk_bot/mods/chatmanager/kick.py
@@ -9,13 +9,16 @@ class Kick(BasicPlug, Upload):
 
     def main(self):
         uid = self.amsg["from_id"]
-        for a in self.vk.messages.getConversationMembers(peer_id=self.event.object.peer_id)['items']:
-            if a['member_id'] == self.event.object.from_id:
-                if 'is_admin' in a:
-                    try:
-                        self.vk.messages.removeChatUser(
-                            chat_id=self.event.chat_id, user_id=uid)
-                    except vk_api.exceptions.ApiError:
-                        self.sendmsg("Может пользователя нет в беседе?")
-                else:
-                    self.sendmsg("Вы не админ беседы!")
+        uadmin = False
+        for a in self.vk.messages.getConversationMembers(peer_id=self.event.object.peer_id,
+                                                         fields="admins")['items']:
+            if a['member_id'] == self.event.object.from_id and a['is_admin']:
+                uadmin = True
+        if uadmin:
+            try:
+                self.vk.messages.removeChatUser(
+                    chat_id=self.event.chat_id, user_id=uid)
+            except vk_api.exceptions.ApiError:
+                self.sendmsg("Может пользователя нет в беседе?")
+        else:
+            self.sendmsg("Вы не админ беседы!")
