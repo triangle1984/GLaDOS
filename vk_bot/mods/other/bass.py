ea9c01f882a86f3434cea1daaffbd112ab01c9e6
anar66 <steam6456@gmail.com>
2019-11-19 19:39:22 +0000

    работать с аудио в вк - ад

--- vk_bot/mods/other/bass.py
+++ vk_bot/mods/other/bass.py
@@ -1,42 +1,43 @@
-from vk_bot.core.modules.basicplug import BasicPlug
-from vk_bot.core.modules.upload import Upload
-from pydub import AudioSegment
-import os
-import random
+#from vk_bot.core.modules.basicplug import BasicPlug
+#from vk_bot.core.modules.upload import Upload
+#from pydub import AudioSegment
+#import os
+#import random
 
 
-class MusicBoost(BasicPlug, Upload):
-    doc = "басуха прям пиздец"
-    command = ["/басуха"]
+# class MusicBoost(BasicPlug, Upload):
+#    doc = "басуха прям пиздец"
+#    command = ["/басуха"]
+#    included = False
 
-    def main(self):
-        try:
-            link = self.event.object["attachments"][0]["audio"]["url"]
-            title = self.event.object["attachments"][0]["audio"]["title"]
-        except:
-            self.sendmsg("Ты забыл скинуть что надо разъебать")
-        os.system(f'wget {link} -O {title}.mp3')
-        #title = self.dowloadfile(link)['name']
-        sound = AudioSegment.from_mp3(f"{title}.mp3")
-        soundname = f"title-{title}.wav"
-        sound.export(soundname, format="wav")
+#    def main(self):
+#        try:
+#            link = self.event.object["attachments"][0]["audio"]["url"]
+#            title = self.event.object["attachments"][0]["audio"]["title"]
+#        except:
+#            self.sendmsg("Ты забыл скинуть что надо разъебать")
+#        os.system(f'wget {link} -O {title}.mp3')
+#        #title = self.dowloadfile(link)['name']
+#        sound = AudioSegment.from_mp3(f"{title}.mp3")
+#        soundname = f"title-{title}.wav"
+#        sound.export(soundname, format="wav")
 
-        sound = AudioSegment.from_file(soundname)
-        # make bassboost track
-        bassboost = sound + 20
-        bass = f"boost-{title}.wav"
-        bassboost.export(bass, format="wav")
+#        sound = AudioSegment.from_file(soundname)
+#        # make bassboost track
+#        bassboost = sound + 20
+#        bass = f"boost-{title}.wav"
+#        bassboost.export(bass, format="wav")
 
-        # Transfer wav to OGG
-        sound = AudioSegment.from_file(bass)
-        ogg = f"boost-{title}.ogg"
-        sound.export(ogg, format="ogg")
-        try:
-            self.sendmsg(f"делаю бах-бах")
-            self.sendmsg("", self.audiomessage(ogg))
-        except:
-            return
-        finally:
-            os.remove(bass)
-            os.remove(soundname)
-            os.remove(f"{title}.mp3")
+#        # Transfer wav to OGG
+#        sound = AudioSegment.from_file(bass)
+#        ogg = f"boost-{title}.ogg"
+#        sound.export(ogg, format="ogg")
+#        try:
+#            self.sendmsg(f"делаю бах-бах")
+#            self.sendmsg("", self.audiomessage(ogg))
+#        except:
+#            return
+#        finally:
+#            os.remove(bass)
+#            os.remove(soundname)
+#            os.remove(f"{title}.mp3")
