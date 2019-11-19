f1a8971dad9fbb12bc50eb58d299ac4acfcc27bf
anar66 <steam6456@gmail.com>
2019-11-19 23:39:10 +0000

    се фиксанули

--- vk_bot/mods/util/calc2.py
+++ vk_bot/mods/util/calc2.py
@@ -10,10 +10,10 @@ class Calculator2(BasicPlug):
         equation = "".join(self.text[1:])
         equation = equation.replace("**", "")
         try:
-            int(re.sub("[+*/-^]", "", equation))
+            int(re.sub(r"[+*-/]", "", equation))
             self.sendmsg(f"Результат: {eval(equation)}")
         except ValueError:
-            self.sendmsg("Тока цифры")
+            self.sendmsg("Использовать в уравнениях можно только +*-/**")
             return
         except ZeroDivisionError:
             self.sendmsg("Планету взорвать хочешь?")
