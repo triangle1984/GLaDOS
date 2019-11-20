e9d285a9111058902c20b015d6c23ae4d94b70fe
anar66 <steam6456@gmail.com>
2019-11-20 20:30:36 +0000

    федя заебал

--- vk_bot/mods/bots/about.py
+++ vk_bot/mods/bots/about.py
@@ -1,16 +1,19 @@
 from vk_bot.core.modules.basicplug import BasicPlug
 from vk_bot import mods
 from boltons import iterutils
+
+
 class AboutBot(BasicPlug):
     doc = "Инфа о боте"
     command = ["/оботе"]
+
     def main(self):
         info = """
         бот написан на python, двумя прогерами
         первый прогер: *slava_a_i_r
         второй прогер: *pythonoglot
         модуль на распознавание гс написал: *feelan03
-        Так же, он опенсурс, репа: github.com/triangle1984/GLaDOS
+        Также, он опенсурс, репа: github.com/triangle1984/GLaDOS
         Приятного пользования~
         Узнать команды: /хелп
         """
