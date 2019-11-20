e7b8df4b5a431070821c6cd624bee74bea59a7e3
anar66 <steam6456@gmail.com>
2019-11-20 20:17:49 +0000

    рефакторинг парсинга

--- vk_bot/mods/parsing/bash.py
+++ vk_bot/mods/parsing/bash.py
@@ -6,12 +6,9 @@ import requests
 class Bash(BasicPlug):
     command = ["/баш"]
     doc = "рандом цитата из баша"
+
     def main(self):
         resp = requests.get("https://bash.im/random")
         soup = BeautifulSoup(resp.text, 'lxml')
         text = soup.find("div", {"class": "quote__body"})
-        text = str(text)
-        text = text.replace('<div class=\"quote__body\">', "")
-        text = text.replace('</div>', "")
-        text = text.replace('<br/>', "\n")
-        self.sendmsg(text)
+        self.sendmsg(text.text)
