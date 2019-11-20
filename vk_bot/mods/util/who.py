686d581e43c235dcbb0455f70eb15727a411a0bc
Ferowenso <ferowenso@yandex.ru>
2019-11-20 14:17:39 +0300

    добавил randomuser в команду /кто

--- vk_bot/mods/util/who.py
+++ vk_bot/mods/util/who.py
@@ -1,15 +1,12 @@
 from vk_bot.core.modules.basicplug import BasicPlug
+from vk_bot.core.modules.othermethods import OtherMethod
 import random
-class Who(BasicPlug):
+class Who(BasicPlug, OtherMethod):
     doc = "Выбирает рандомного участника беседы под вашим предлогом"
     command = ["/кто"]
     def main(self):
         try:
             whotext = ' '.join(self.text[1:])
-            whoid = random.choice(self.vk.messages.getConversationMembers(peer_id=self.event.object.peer_id)['profiles'])
-            whofirstname = whoid['first_name']
-            wholastname = whoid['last_name']
-            whoidstr = whoid['id']
-            self.sendmsg(f"Кто {whotext}? Я думаю, это @id{whoidstr} ({whofirstname} {wholastname})")
+            self.sendmsg(f"Кто {whotext}? Я думаю, это {self.randomuser()}")
         except:
             self.sendmsg("Для работы этой команды боту нужна админка в беседе!")
