3ac962f0d16c6f8786f0e8a5d6a0a7ada28ecb2f
anar66 <steam6456@gmail.com>
2019-11-21 22:07:54 +0000

    гуглбаба. Можт еще доработаю

---
+++ vk_bot/mods/other/text_to_speech.py
@@ -0,0 +1,15 @@
+from vk_bot.core.modules.basicplug import BasicPlug
+from vk_bot.core.modules.upload import Upload
+from gtts import gTTS
+import time
+
+
+class TextToAudio(BasicPlug, Upload):
+    doc = "гс в текст"
+    command = ["/озвучить", "/гуглбаба"]
+
+    def main(self):
+        name = f"{time.time_ns()}.mp3"
+        tts = gTTS(",".join(self.text[1:]), lang='ru')
+        tts.save(name)
+        self.sendmsg("Дэржите", self.audiomessage(name))
