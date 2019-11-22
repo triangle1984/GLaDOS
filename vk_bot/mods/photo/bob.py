69f1b93f1f198367e68ed667ce7f10357764459a
Ferowenso <ferowenso@yandex.ru>
2019-11-22 19:24:50 +0300

    команда /боб

---
+++ vk_bot/mods/photo/bob.py
@@ -0,0 +1,26 @@
+from PIL import Image, ImageDraw
+from loadevn import *
+import io, requests, random, os
+from vk_bot.core.modules.basicplug import BasicPlug
+from vk_bot.core.modules.upload import Upload
+class Bob(BasicPlug, Upload):
+    doc = "вашу пикчу нарисует сам Боб Росс!"
+    command = ["/боб"]
+    def main(self):
+        url = self.event.object['attachments'][0]['photo']['sizes'][-1]['url']
+        img = requests.get(url).content
+        f = io.BytesIO(img)
+        image = Image.open(f)
+        resized_img = image.resize((448, 447), Image.ANTIALIAS)
+        bob = Image.open('/usr/local/bin/testing/GLaDOS/vk_bot/mods/testing/bob.png')
+        resized_img = resized_img.convert('RGBA')
+        paint = Image.open('/usr/local/bin/testing/GLaDOS/vk_bot/mods/testing/e2e46dbe6f89b13bcdcfaafa883753f9.jpg')
+        paint.paste(resized_img, (109, 40), resized_img)
+        paint.paste(bob, (0, 0), bob)
+        name = f"name{random.randint(0, 1000)}.png"
+        paint.save(name)
+        try:
+            attachment  = self.uploadphoto(name)
+            self.sendmsg("Дэржите фотку", attachment)
+        finally:
+            os.remove(name)
