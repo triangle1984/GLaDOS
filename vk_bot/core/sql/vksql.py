a0d443b8f1233864370e7a48e3913c03c066e9db
anar66 <steam6456@gmail.com>
2019-11-22 15:16:53 +0000

    говнокод из бд-кода пора выбрасывать, а то и пыль сдуавать

--- vk_bot/core/sql/vksql.py
+++ vk_bot/core/sql/vksql.py
@@ -2,67 +2,61 @@ import pymysql
 from pymysql.cursors import DictCursor
 from contextlib import closing
 from vk_bot.loadevn import *
+
+
 def auth():
     conn = pymysql.connect(host=ip,
-                             user=user,
-                             password=password,
-                             db=db,
-                             cursorclass=DictCursor)
+                           user=user,
+                           password=password,
+                           db=db,
+                           cursorclass=DictCursor)
     return conn
+
+
 def tablecount(table, value, count):
-    try:
-        conn = auth()
-        with conn.cursor() as cursor:
-            query = f"SELECT COUNT(*) FROM {table} WHERE {value} = '{count}'"
-            cursor.execute(query)
-            return cursor.fetchone()["COUNT(*)"]
-    except:
-        return
+    conn = auth()
+    with conn.cursor() as cursor:
+        query = f"SELECT COUNT(*) FROM {table} WHERE {value} = '{count}'"
+        cursor.execute(query)
+        return cursor.fetchone()["COUNT(*)"]
+
+
 def checktable(table, value, should, andd=False):
-    try:
-        conn = auth()
-        with conn.cursor() as cursor:
-            query = f"SELECT * FROM {table} WHERE {value} = '{should}'"
-            if andd:
-                query = f"SELECT * FROM {table} WHERE {value} = '{should}' and {andd}"
-            cursor.execute(query)
-        return cursor.fetchone()
-    except:
-        return
+    conn = auth()
+    with conn.cursor() as cursor:
+        query = f"SELECT * FROM {table} WHERE {value} = '{should}'"
+        if andd:
+            query = f"SELECT * FROM {table} WHERE {value} = '{should}' and {andd}"
+        cursor.execute(query)
+    return cursor.fetchone()
+
+
 def tableadd(table, value, add, one=False):
-    try:
-        conn = auth()
-        try:
-            with conn.cursor() as cursor:
-                query = f"INSERT INTO {table} ({value}) VALUES ({add})"
-                if one:
-                    if bool(checktable(table, value, add)):
-                        return
-                cursor.execute(query)
-                conn.commit()
-        except pymysql.err.InternalError:
-            return
-    except:
-        return
+    conn = auth()
+    with conn.cursor() as cursor:
+        query = f"INSERT INTO {table} ({value}) VALUES ({add})"
+        if one:
+            if bool(checktable(table, value, add)):
+                return
+        cursor.execute(query)
+        conn.commit()
+
+
 def tablerm(table, value, rm, andd=False):
-    try:
-        conn = auth()
-        with conn.cursor() as cursor:
-            query = f"DELETE FROM {table} WHERE {value} = '{rm}'"
-            if andd:
-                query = f"DELETE FROM {table} WHERE {value} = '{rm}' and {andd}"
-            cursor.execute(query)
-            conn.commit()
-    except:
-        return
+    conn = auth()
+    with conn.cursor() as cursor:
+        query = f"DELETE FROM {table} WHERE {value} = '{rm}'"
+        if andd:
+            query = f"DELETE FROM {table} WHERE {value} = '{rm}' and {andd}"
+        cursor.execute(query)
+        conn.commit()
+
+
 def tableupdate(table, value, should, where, add=False):
-    try:
-        conn = auth()
-        with conn.cursor() as cursor:
-            query = f"UPDATE {table} SET {value} = '{should}' WHERE {where}"
-            if add:
-                query = f"UPDATE {table} SET {value} = ({should}) WHERE {where}"
-            cursor.execute(query)
-            conn.commit()
-    except:
-        return
+    conn = auth()
+    with conn.cursor() as cursor:
+        query = f"UPDATE {table} SET {value} = '{should}' WHERE {where}"
+        if add:
+            query = f"UPDATE {table} SET {value} = ({should}) WHERE {where}"
+        cursor.execute(query)
+        conn.commit()
