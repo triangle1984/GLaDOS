b2397c8349046e276a6c080592e31cf6353a758c
anar66 <steam6456@gmail.com>
2019-11-22 21:43:44 +0000

    вернул эту дичь

--- vk_bot/mods/other/speechtotext.py
+++ vk_bot/mods/other/speechtotext.py
@@ -32,7 +32,8 @@ class AutdiotoText(BasicPlug, Upload):
             result = r.recognize_google(audio, language="ru_RU")
             self.sendmsg(f"сказал: {result}")
         except sr.UnknownValueError:
-            return
+            self.sendmsg(
+                f"НИХУЯ НЕ ПОНИМАЮ НИ ОДНОГО СЛОВА БЛЯТЬ, КАРТАВАЯ СУКА")
         except sr.RequestError:
             self.sendmsg(f"Ошибка при отправки запроса")
         finally:
