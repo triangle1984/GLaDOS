4f1fc83de2a70acc130f56f7444b51368ffbb334
Ferowenso <ferowenso@yandex.ru>
2019-11-24 15:15:34 +0300

    все блять заебали эти коммиты

--- vk_bot/core/sql/sqlgame.py
+++ vk_bot/core/sql/sqlgame.py
@@ -15,7 +15,7 @@ def sendall(event, text, vk, attachment=None):
         result = cursor.fetchall()
         for a in result:
             if checktable(f'{tablechat}', "id", f"{a['id']}", andd="whitelist = 1"):
-                print(".")
+                None
             else:    
                 try:
                     vk.messages.send(chat_id=a["id"], random_id=get_random_id(),
