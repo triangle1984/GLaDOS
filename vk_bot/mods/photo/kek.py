3dcdc852aa573eb8dd1ec7cb8a05309cd8a4546f
Ferowenso <ferowenso@yandex.ru>
2019-11-25 19:21:55 +0300

    команда /кек

---
+++ vk_bot/mods/photo/kek.py
@@ -0,0 +1,32 @@
+from PIL import Image, ImageDraw
+from loadevn import *
+import io
+import requests
+import random
+import os
+from vk_bot.core.modules.basicplug import BasicPlug
+from vk_bot.core.modules.upload import Upload
+
+
+class Quote(BasicPlug, Upload):
+    doc = "Фильтр кек"
+    command = ["/кек"]
+
+    def main(self):
+        url = self.event.object['attachments'][0]['photo']['sizes'][-1]['url']
+        img = requests.get(url).content
+        f = io.BytesIO(img)
+        image = Image.open(f)
+        width, height = image.size
+        if not width%2 == 0:
+            width-=1
+        kek = Image.new("RGB", (width, height))
+        wid = round(int(width / 2))
+        img = image.crop((0, 0, wid, height))
+        mirror = img.transpose(Image.FLIP_LEFT_RIGHT)
+        kek.paste(img, (0,0), img.convert('RGBA'))
+        kek.paste(mirror, (int(wid), 0), mirror.convert('RGBA'))
+        name = f"name{random.randint(0, 1000)}.jpg"
+        kek.save(name)
+        attachment = self.uploadphoto(name)
+        self.sendmsg("Дэржите фотку", attachment)
