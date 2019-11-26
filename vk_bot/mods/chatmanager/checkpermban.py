46b365aad23c3ba74659f870c4dc6775c2c8730c
Ferowenso <ferowenso@yandex.ru>
2019-11-26 19:00:09 +0300

    ну начало пермачу положено

--- vk_bot/mods/chatmanager/checkpermban.py
+++ vk_bot/mods/chatmanager/checkpermban.py
@@ -1,10 +1,15 @@
 from vk_bot.core.modules.basicplug import BasicPlug
+from vk_bot.core.modules.chatmager import ChatManager
+from loadevn import permban
+from vk_bot.core.sql.vksql import *
 
-
-class CheckPermban(BasicPlug):
+class CheckPermban(BasicPlug, ChatManager):
     types = "runalways"
     action = "chat_invite_user"
 
     def main(self):
-        print(self.event.object.action['member_id'])
-        print(self.peer_id)
+        uid = self.event.object.action['member_id']
+        chat_id = self.event.chat_id
+        if checktable(f"{permban}", "chat_id", f"{chat_id}", andd=f"uid = {uid}"):
+            self.kick(uid)
+            
---
+++ vk_bot/mods/chatmanager/permban.py
@@ -0,0 +1,21 @@
+import vk_api
+from vk_bot.core.modules.basicplug import BasicPlug
+from vk_bot.core.modules.chatmager import ChatManager
+from loadevn import permban
+from vk_bot.core.sql.vksql import *
+
+class Permban(BasicPlug, ChatManager):
+    doc = "Пермбан"
+    command = ("пермбан",)
+
+    def main(self):
+        if len(self.text) > 1:
+            uid = self.returnpusuid(self.text[1])[0]
+        elif self.amsg:
+            uid = self.amsg["from_id"]
+        else:
+            self.sendmsg("каво кикать")
+            return
+        self.kick(uid)
+        if not checktable(f"{permban}", "chat_id", f"{self.event.chat_id}", andd=f"uid = {uid}"):
+            tableadd(f'{permban}', "uid, chat_id", f"{uid}, {self.event.chat_id}") 
