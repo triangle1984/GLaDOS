4cde3d511d4aabc71d06933c912c3f985558d1fa
anar66 <steam6456@gmail.com>
2019-11-26 23:19:37 +0000

    Ай ля, забыл

--- vk_bot/core/sql/vksql.py
+++ vk_bot/core/sql/vksql.py
@@ -66,5 +66,7 @@ def tableupdate(table, value, should, where, add=False):
         query = f"UPDATE {table} SET {value} = '{should}' WHERE {where}"
         if add:
             query = f"UPDATE {table} SET {value} = ({should}) WHERE {where}"
+        if "#" in list(query):
+            return
         cursor.execute(query)
         conn.commit()
