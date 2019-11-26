458dbac4ea9abf63652cad7a6ee8c36870a2b382
anar66 <steam6456@gmail.com>
2019-11-26 14:15:17 +0000

    доработал калькулятор малеха

--- vk_bot/mods/util/calc2.py
+++ vk_bot/mods/util/calc2.py
@@ -9,10 +9,10 @@ class Calculator2(BasicPlug):
     def main(self):
         equation = "".join(self.text[1:])
         try:
-            int(re.sub(r"[()+*-/]", "", equation))
+            int(re.sub(r"[()+*-/e]", "", equation))
             self.sendmsg(f"Результат: {eval(equation)}")
         except ValueError:
-            self.sendmsg("Использовать в уравнениях можно только +*-/")
+            self.sendmsg("Использовать в уравнениях можно только +*-/()e")
             return
         except ZeroDivisionError:
             self.sendmsg("Планету взорвать хочешь?")
