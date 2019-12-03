c274493988a2c366cddbc8fbab4de36ed5237fe6
anar66 <steam6456@gmail.com>
2019-12-03 15:01:20 +0000

    держи порцию пеп8

--- vk_bot/mods/testing/relation.py
+++ vk_bot/mods/testing/relation.py
@@ -1,9 +1,12 @@
 from vk_bot.core.modules.basicplug import BasicPlug
 from vk_bot.core.sql.vksql import *
 import pylibmc
+
+
 class Relation(BasicPlug):
     doc = "Отношения"
     command = ("отношения2",)
+
     def main(self):
         try:
             requests = self.text[1]
@@ -13,10 +16,11 @@ class Relation(BasicPlug):
                 self.accept()
             elif requests == 'отказать':
                 self.deny()
-            elif requests == "стат":        
+            elif requests == "стат":
                 self.statrelation()
         except:
             pass
+
     def accept(self):
         uid2 = self.mc.get(f"r{self.uid}")
         uid = self.uid
@@ -25,34 +29,41 @@ class Relation(BasicPlug):
             tableadd('relation', "id, id2", f'{uid}, {uid2}')
             self.mc.delete(f"r{self.uid}")
         else:
-            self.sendmsg("запросов не поступало"); return
+            self.sendmsg("запросов не поступало")
+            return
+
     def deny(self):
-        msg = "ты отклонил предложение"     
+        msg = "ты отклонил предложение"
         if self.mc.delete(f"r{self.uid}") == False:
             msg = "запросов не поступало"
         self.mc.delete(f"r{self.uid}")
         self.sendmsg(msg)
+
     def offer(self):
         uid2 = self.returnpusuid(self.text[2][3:])[0]
         if int(uid2) == self.uid:
-            self.sendmsg("низя отправить предложение самому себе"); return
+            self.sendmsg("низя отправить предложение самому себе")
+            return
         if self.mc.get(f"r{uid2}"):
-            self.sendmsg("вы уже отправили запрос"); return
+            self.sendmsg("вы уже отправили запрос")
+            return
         elif checktable("relation", "id2 or id", self.uid):
-            self.sendmsg("ты уже встречаешься"); return
-        else: 
+            self.sendmsg("ты уже встречаешься")
+            return
+        else:
             self.mc.set(f"r{uid2}", self.uid, time=120)
             nameid = f"*id{uid2}({self. vk.users.get(user_ids=uid2, name_case='dat')[0]['first_name']})"
             self.sendmsg(f"Ты предложил встречаться {nameid}")
 
     def statrelation(self):
         if not checktable("relation", "id2 or id", self.uid):
-            self.sendmsg("ни с кем не встречаешься"); return
+            self.sendmsg("ни с кем не встречаешься")
+            return
         else:
             user = checktable("relation", "id2 or id", self.uid)
             if user['id'] == self.uid:
                 user = user['id2']
             else:
                 user = user['id']
-            nameid = f"*id{user}({self. vk.users.get(user_ids=user, name_case='ins')[0]['first_name']})"    
+            nameid = f"*id{user}({self. vk.users.get(user_ids=user, name_case='ins')[0]['first_name']})"
             self.sendmsg(f"Ты встречаешься с {nameid}")
