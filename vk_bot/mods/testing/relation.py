1937952dc7a5df3e3877217e5bde5aad43076983
Ferowenso <ferowenso@yandex.ru>
2019-12-03 14:00:59 +0300

    гатова

--- vk_bot/mods/testing/relation.py
+++ vk_bot/mods/testing/relation.py
@@ -3,7 +3,7 @@ from vk_bot.core.sql.vksql import *
 import pylibmc
 class Relation(BasicPlug):
     doc = "Отношения"
-    command = ("отношения",)
+    command = ("отношения2",)
     def main(self):
         try:
             requests = self.text[1]
@@ -21,25 +21,33 @@ class Relation(BasicPlug):
         uid2 = self.mc.get(f"r{self.uid}")
         uid = self.uid
         if uid2:
+            self.sendmsg("ты принял предложение!")
             tableadd('relation', "id, id2", f'{uid}, {uid2}')
             self.mc.delete(f"r{self.uid}")
         else:
-            self.sendmsg("запросов не поступало")
+            self.sendmsg("запросов не поступало"); return
     def deny(self):
-        self.mc.delete(f"r{self.uid}")     
+        msg = "ты отклонил предложение"     
         if self.mc.delete(f"r{self.uid}") == False:
-            self.sendmsg("запросов не поступало")
+            msg = "запросов не поступало"
+        self.mc.delete(f"r{self.uid}")
+        self.sendmsg(msg)
     def offer(self):
         uid2 = self.returnpusuid(self.text[2][3:])[0]
+        if int(uid2) == self.uid:
+            self.sendmsg("низя отправить предложение самому себе"); return
         if self.mc.get(f"r{uid2}"):
-            self.sendmsg("вы уже отправили запрос")
-        if checktable("relation", "id2 or id", self.uid):
-            self.sendmsg("ты уже встречаешься")
+            self.sendmsg("вы уже отправили запрос"); return
+        elif checktable("relation", "id2 or id", self.uid):
+            self.sendmsg("ты уже встречаешься"); return
         else: 
             self.mc.set(f"r{uid2}", self.uid, time=120)
+            nameid = f"*id{uid2}({self. vk.users.get(user_ids=uid2, name_case='dat')[0]['first_name']})"
+            self.sendmsg(f"Ты предложил встречаться {nameid}")
+
     def statrelation(self):
         if not checktable("relation", "id2 or id", self.uid):
-            self.sendmsg("ни с кем не встречаешься")
+            self.sendmsg("ни с кем не встречаешься"); return
         else:
             user = checktable("relation", "id2 or id", self.uid)
             if user['id'] == self.uid:
