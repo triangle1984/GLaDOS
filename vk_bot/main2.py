f8a6cd30d4bb1fb3c31801d5427ec883beb24f91
feelan <feelan03@gmail.com>
2019-12-14 22:37:10 +0000

    Вот так?

--- vk_bot/main2.py
+++ vk_bot/main2.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3.7
-from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
-from vk_api import VkUpload
-from loadevn import *
-from vk_bot.core.sql.vksql import *
-from vk_bot.core.utils.botutil import *
-from concurrent.futures import ThreadPoolExecutor, wait, as_completed
-import pylibmc
-import vk_api
-import logging
-import datetime
-from vk_bot.core.sql.sqlgame import *
-import mods
 import argparse
+import datetime
+import logging
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+import pylibmc
+from vk_api import VkUpload
+from vk_api.bot_longpoll import VkBotLongPoll
+
+import mods
+from vk_bot.core.utils.botutil import *
 
 
 class Main:
@@ -61,6 +59,7 @@ class Main:
         args = argparse.ArgumentParser(description="параметры запуска бота")
         args.add_argument('-d', '--debug',  action='store_true',
                           default=False, dest="debug")
+
         args = args.parse_args()
         self.debug = args.debug
 
@@ -130,7 +129,6 @@ class Main:
             if requests[0] in preixcommand:
                 requests2 = requests[1:]
                 uberequests2 = event.object.text[1:].lower()
-            uberequests = event.object.text.lower()
         except IndexError:
             pass
         """
@@ -176,7 +174,7 @@ class Main:
                                     prefix=prefix, peer=event.object.peer_id, mc=self.mc,
                                     rtext=event.object.text)
                     module.makeothervariables()
-                    if not module.init_threading:
+                    if not module.thread:
                         then = datetime.datetime.now()
                         logging.info(f"Запуск модуля {module.__module__}")
                         module.main()
